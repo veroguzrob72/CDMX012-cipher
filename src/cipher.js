acee3101a3f2d899e8a43ef58b7ceb589d6f8d0d
veroguzrob72 <vgrdesd@gmail.com>
2022-01-28 14:34:45 -0600

    cifrado y decifrado funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,39 @@
 const cipher = {
-  // ...
-};
+  encode: function (numeroSaltos, palabraSimple,) {
+    let palabraFinal= '';
+    for (let a=0; a<palabraSimple.length; a++){
+      let codAscii = palabraSimple.charCodeAt(a);
+      if (codAscii >=65 && codAscii<= 90) {
+        let cifradoCesar = ((codAscii - 65 + parseInt(numeroSaltos)) % 26 + 65)
+        let newAscii = String.fromCharCode(cifradoCesar)
+        palabraFinal+=newAscii
+      }
+      else {
+        palabraFinal = palabraFinal + palabraSimple[a];
+      }
+    }
+    return palabraFinal;
+  },
 
+  decode: function (numeroModif, palabraCod) {
+    let mnsjFinal2= '';
+    for (let i=0; i<palabraCod.length; i++){
+      let codAscii2 = palabraCod.charCodeAt(i);
+      if (codAscii2 >=65 && codAscii2 <=90) {
+        let cipherCesar = ((codAscii2 - 90 - parseInt(numeroModif)) % 26 + 90)
+        let newAscii2 = String.fromCharCode(cipherCesar)
+        mnsjFinal2+=newAscii2
+      } 
+      else {
+        mnsjFinal2 = mnsjFinal2 + palabraCod[i];
+      }
+
+    }
+      
+    return mnsjFinal2;
+
+  }
+
+};
+  
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const botonCifrar = document.getElementById('btnCipher');
+    botonCifrar.addEventListener('click', function () {
+        let palabraSimple = document.getElementById('wordSimple').value.toUpperCase();
+        let numeroSaltos = document.getElementById('numberCod').value;
+        let palabraCifrada = document.getElementById('wordCipher');
+            palabraCifrada.innerHTML = cipher.encode(numeroSaltos, palabraSimple)
+                    
+   }  
+
+)
+
+const botonDecifrar = document.getElementById('btndecipher');
+    botonDecifrar.addEventListener('click', function () {
+        let palabraCod = document.getElementById('wordCod').value.toUpperCase();
+        let numeroModif = document.getElementById('numberCod2').value;
+        let palabraDecifrada = document.getElementById('message');
+            palabraDecifrada.innerHTML = cipher.decode(numeroModif, palabraCod)
+                    
+   }  
+
+)
